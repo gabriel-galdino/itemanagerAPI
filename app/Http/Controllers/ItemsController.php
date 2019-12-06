a96fe1471a0884eed420e0523b6f530d1a6d8fd0
Gabriel Galdino <gabrielgaldino7@yahoo.com>
2019-12-05 22:21:53 -0300

    Cria métodos get/items e get/item/

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Item;
 
 class ItemsController extends Controller
 {
@@ -14,6 +15,8 @@ class ItemsController extends Controller
     public function index()
     {
         //
+        $items = Item::all();
+        return response()->json($items);
     }
 
     /**
@@ -46,6 +49,8 @@ class ItemsController extends Controller
     public function show($id)
     {
         //
+        $item = Item::find($id);
+        return response()->json($item);
     }
 
     /**
