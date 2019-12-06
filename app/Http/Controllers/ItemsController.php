f9fcc5ecfe6b2a27c326dbce6357942e71d0a0b7
Gabriel Galdino <gabrielgaldino7@yahoo.com>
2019-12-05 23:04:51 -0300

    Adiciona alterar informações do item

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -90,6 +90,21 @@ class ItemsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $validator = Validator::make($request->all(), [
+          'text' => 'required'
+        ]);
+
+        if($validator->fails()){
+          $response = array('response' => $validator->messages(), 'success' => false);
+          return $response;
+        }
+        else {
+          $item = Item::find($id);
+          $item->text = $request->input('text');
+          $item->body = $request->input('body');
+          $item->save();
+          return response()->json($item);
+        }
     }
 
     /**
